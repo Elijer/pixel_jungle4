a45580e1f03a9b9265bc869d213be941e411bcd8
Elijah Kennedy <elijahclimbs@gmail.com>
2025-03-01 21:34:02 -0500

    transferring grid with buffer but index issues

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import setupServer from './lib/setupServer.js';
 import { log } from './lib/logger.js';
-import { handlePlantLifecycle, createPlant } from './lib/game.js';
+import { handlePlantLifecycles, createPlant, getViewAsBuffer } from './lib/game.js';
 const { io, port, httpServer } = setupServer();
 
-console.time("5mil")
-
-for (let i=0; i < 10000; i++){
-  createPlant(0, 16)
-}
+createPlant(0, 6500) // at midpoint
 
 setInterval(()=>{
-  handlePlantLifecycle()
+  handlePlantLifecycles()
+  // sending a pre-ordained section of the map - section 1
+  // but later on this will be triggered
+  // by changing views only
+  const buff = getViewAsBuffer(4096)
+  io.emit("view", new Uint8Array(buff)); // 4096 value is an example of a player who is actually in the first view, since it's the first position of the second row of the master grid
   // Around here, I have to actually send some data
-}, 100)
+}, 500)
 
 httpServer.listen(port, () => {
   log(`-->>> STARTED SERVER: ${port} <<<--`)
--- src/lib/game.ts
+++ src/lib/game.ts
@@ -7,31 +7,45 @@ type Level = 0 | 1 | 2
 type Lifespan = number
 type Position = number
 
-const lifespanArchectypes = [
+const lifespanArchetypes = [
   16, 32, 64 // measured in 100s of ms
 ]
+
+function splitUp64(num: number): (0 | 1 | 2 | 3) {
+  // values should be 0-63 inclusive, a total of 64 values
+  // So in this case, 0 for lifespan or for energy is synonymous with nonexistence, not the step before it
+  if (num > 31) return 3
+  if (num > 15) return 2
+  if (num > 0) return 1
+  return 0
+}
 
 function getMapConfig(){
   const rows = 64
   const cols = 64
   const viewRows = 4
   const viewCols = 4
+  const totalRows = viewRows * rows
+  const totalCols = viewCols * cols
   const views = 16
-  const positions = rows * cols * views
-  return {rows, cols, viewRows, viewCols, views, positions}
+  const positionsPerView = rows * cols
+  const positions = positionsPerView * views
+  return {rows, cols, viewRows, viewCols, views, totalCols, totalRows, positionsPerView, positions}
 }
 
 const map = getMapConfig()
 
+// I think technically, at edges of maps when used, this will hop the map in a few cases
+// but maybe it's fine for now
 const compass = [
-  -map.cols - 1,
-  -map.cols,
-  -map.cols + 1,
-  -1,
-  1,
-  map.cols - 1,
-  map.cols,
-  map.cols + 1,
+  -257,
+  // -map.totalCols,
+  // -map.totalCols + 1,
+  // -1,
+  // 1,
+  // map.totalCols - 1,
+  // map.totalCols,
+  // map.totalCols + 1,
 ]
 
 // TODO: Maybe add a limit to the number of entities allowed?
@@ -77,7 +91,7 @@ function createEntity(): Entity {
   try {
     const entity = getEntityId()
     entities.add(entity)
-    console.log(`created ${entity}`)
+    // console.log(`created ${entity}`)
     // log(`created ${entity}`)
     return entity
   } catch (e){
@@ -102,7 +116,7 @@ function removeEntityEntirely(entity: Entity){
     entities.delete(entity)
 
     recycleEntity(entity)
-    console.log(`removed ${entity} entirely`)
+    // console.log(`removed ${entity} entirely`)
     // log(`removed ${entity} entirely`)
 
   } catch (e){
@@ -182,10 +196,6 @@ function createSpatialEntity(position: Position): Entity {
   try {
     const entity = createEntity()
     addPosition(entity, position)
-    positions[entity] = position
-    const entityAtPosition = entitiesByPosition[entity]
-    if (!entityAtPosition) entitiesByPosition[entity] = new Set()
-    entitiesByPosition[entity]?.add(entity)
     return entity
   } catch (e){
     throw new Error(`Error while creating entity createEntity(): ${e}`)
@@ -208,12 +218,17 @@ function removeSocket(entity: Entity): void {
 // PLANTS
 export function createPlant(level: Level, position: Position | undefined = undefined, ): void {
   try {
+    console.log("CREATING PLANT AT POS", position)
+    
+    // prevent possibility of multiple plants in the same place
+    if (position && inhabitantsAtPosition(position)) return
+
     let entity = spawnSpatialEntity(position)
 
     // this can happen if there's no room - the -1 trickles up, and we fail to create a plant
     if (entity === -1) return
     levels[entity] = level
-    const lifespan = lifespanArchectypes[level]
+    const lifespan = lifespanArchetypes[level]
     lifespans[entity] = lifespan
 
     
@@ -221,7 +236,9 @@ export function createPlant(level: Level, position: Position | undefined = undef
     for (let i = 0; i < 2; i++){
       // Get a random timestamp within organism's lifespan
       // TODO: add minerals to influence this
-      childBirthtimes.push(Math.floor(lifespan * Math.random()))
+      // The idea here is that I want variation in lifespans, but I don't want plants
+      // having a chance to reproduce milliseconds after being born, that's weird
+      childBirthtimes.push(Math.floor((lifespan/3) + Math.random() * lifespan / 3))
     }
 
     // later birthtime first so we can pop off the smaller one from the end
@@ -251,21 +268,31 @@ function getNeighborPositions(position: Position): Position[] {
   for (const dir of compass){
     const potentialNeighbor = position + dir
     if (potentialNeighbor >= 0 && potentialNeighbor < map.positions){
-      potentialNeighbors.push(position + dir)
+      if (!inhabitantsAtPosition(potentialNeighbor)){
+        potentialNeighbors.push(potentialNeighbor)
+      }
     }
   }
+  // console.log("starting position", position)
+  // console.log(potentialNeighbors)
   return potentialNeighbors
 }
 
-function lifespanAtPosition(position: Position): number {
+function inhabitantsAtPosition(position: Position): boolean {
   const inhabitants = entitiesByPosition[position]
-  if (inhabitants){
-    for (const inhabitant of inhabitants){
-      if (typeof lifespans[inhabitant] === "number") return lifespans[inhabitant]
-    }
-  }
-  return 0
+  if (inhabitants) return true
+  return false
 }
+
+// function lifespanAtPosition(position: Position): number {
+//   const inhabitants = entitiesByPosition[position]
+//   if (inhabitants){
+//     for (const inhabitant of inhabitants){
+//       if (typeof lifespans[inhabitant] === "number") return lifespans[inhabitant]
+//     }
+//   }
+//   return 0
+// }
 
 function plantReproduce(entity: Entity): void {
 
@@ -275,23 +302,27 @@ function plantReproduce(entity: Entity): void {
   if (!position) throw new Error(`plantReproduce failed to get parent location ${entity}`)
 
   const level = levels[entity]!
-  const lifespan = lifespanArchectypes[level]
+  const lifespan = lifespanArchetypes[level]
   if (!lifespan) throw new Error(`plantReproduce failed to get level of parent`)
 
   const seedPositions: Position[] = []
   const neighborPositions = getNeighborPositions(position)
   for (const neighboringPosition of neighborPositions){
-    if (lifespanAtPosition(neighboringPosition) / 3 >= lifespan) continue
+    // if (lifespanAtPosition(neighboringPosition) / 3 >= lifespan) continue
+    if (inhabitantsAtPosition(neighboringPosition)) continue
     seedPositions.push(neighboringPosition)
   }
+
+  if (!seedPositions.length) return
 
   const randomSeedPosition = seedPositions[Math.floor(Math.random() * seedPositions.length)]
   createPlant(level, randomSeedPosition)
 }
 
-export function handlePlantLifecycle(): void {
+export function handlePlantLifecycles(): void {
 
   for (let entity = 0; entity < lifespans.length; entity++){
+
     const lifespan = lifespans[entity]
     if (lifespan === undefined) continue
 
@@ -313,3 +344,107 @@ export function handlePlantLifecycle(): void {
     decrementLifespan(entity)
   }
 }
+
+// NETWORK ABSTRACTIONS
+
+
+// TODO: This should return a buffer representation of the view of the position given
+// There are 16 views
+// However, we should just keep the most up to date version of this saved in memory
+// and update it as things save
+// That way, incremental changes in global state change the global view representations once
+// and all this method will have to do is get the right view slice and serve it
+// but as it is, the view will have to be computed whenever a player moves, which is more expensive
+// in probably just about any case, even with just one player - it's also a lot of computation at once
+export function getViewAsBuffer(position: number): Buffer {
+  const viewSize = 4096; // 64x64 view
+  const bitDepth = 2; // Each value needs 2 bits
+  const bytesNeeded = viewSize / (8 / bitDepth); // 1024 bytes
+  const buffer = Buffer.alloc(bytesNeeded);
+
+  // Determine which view they are in
+  const view = Math.floor(position / map.positionsPerView);
+  const viewCol = view % 4; // Which fourth of each row this view corresponds to
+  const firstRowInView = Math.floor(view / 4) * 64; // Find the first row
+  // const firstPositionInView = view * map.positionsPerView;
+
+  let tempArray: (0 | 1 | 2 | 3)[] = Array.from({length: viewSize}, ()=>0)
+
+  // Extract values for the 64x64 section
+  for (let row = 0; row < 64; row++){
+    for (let col = 0; col < 64; col++){
+      const mapRow = firstRowInView + row
+      const mapCol = viewCol * 64 + col
+      const pos = mapRow * 256 + mapCol
+
+      const entitiesAtPosition = entitiesByPosition[pos];
+      if (entitiesAtPosition?.size) {
+  
+        for (let entity of entitiesAtPosition) {
+  
+          // aminal
+          // don't have any of these yet
+          // const energy = energies[entity];
+          // if (typeof energy === "number") {
+          //   tempArray[i] = splitUp64(energy) as 0 | 1 | 2 | 3;
+          //   break;
+          // }
+  
+          // plont
+          const lifespan = lifespans[entity];
+          if (typeof lifespan === "number") {
+            tempArray[row * map.cols + col] = splitUp64(lifespan) as 0 | 1 | 2 | 3;
+          }
+        }
+      }
+    }
+  }
+
+
+  // for (let i = 0; i < viewSize; i++) {
+  //   const pos = firstPositionInView + i
+
+  //   const entitiesAtPosition = entitiesByPosition[pos];
+  //   if (entitiesAtPosition?.size) {
+
+  //     for (let entity of entitiesAtPosition) {
+
+  //       // aminal
+  //       // don't have any of these yet
+  //       // const energy = energies[entity];
+  //       // if (typeof energy === "number") {
+  //       //   tempArray[i] = splitUp64(energy) as 0 | 1 | 2 | 3;
+  //       //   break;
+  //       // }
+
+  //       // plont
+  //       const lifespan = lifespans[entity];
+  //       if (typeof lifespan === "number") {
+  //         tempArray[i] = splitUp64(lifespan) as 0 | 1 | 2 | 3;
+  //       }
+  //     }
+  //   }
+  // }
+
+  // Pack the values into the buffer
+  for (let i = 0; i < 1024; i++) {
+    const v1 = tempArray[i * 4] & 0b11; // bitwise AND is wild
+    const v2 = tempArray[i * 4 + 1] & 0b11;
+    const v3 = tempArray[i * 4 + 2] & 0b11;
+    const v4 = tempArray[i * 4 + 3] & 0b11;
+    buffer[i] = (v1 << 6) | (v2 << 4) | (v3 << 2) | v4;
+    
+  }
+
+  return buffer;
+}
+
+
+
+///
+
+// function printBytesInBinary(buff: Buffer){
+//   const binaryStrings = Array.from(buff)
+//     .map(byte => byte.toString(2).padStart(8, '0')); // pad to ensure 8-bit representation
+//   console.log(binaryStrings.join(' ')); // Print the binary representation of each byte
+// }
